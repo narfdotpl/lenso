5c7d2a39e9bfa274bf7a903f303f2f279331332a
Maciej Konieczny <hello@narf.pl>
2016-01-06 14:46:43 +0100

    removed .py extension

--- tests.py
+++ tests.py
@@ -3,10 +3,8 @@
 
 import unittest
 
-import lenso
-# after `mv lenso.py lenso`:
-# from imp import load_source
-# lenso = load_source('lenso', 'lenso')
+from imp import load_source
+lenso = load_source('lenso', 'lenso')
 
 
 class Tests(unittest.TestCase):
